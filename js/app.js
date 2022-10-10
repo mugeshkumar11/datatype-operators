d6e24098ba48f3f11fa9e8067dca24adcd19ae49
mugeshkumar <kmugesh11199@gmail.com>
2022-10-10 19:42:38 +0530

    add new file

---
+++ js/app.js
@@ -0,0 +1,109 @@
+//operators
+//assign operators
+var a =10;
+var b =5;
+//assign
+console.log(a=b);
+//addition
+console.log(a+=b);
+//subtraction
+console.log(a-=b);
+//mulitplication
+console.log(a*=b)
+//division
+console.log(a/=b);
+//modulus
+console.log(a%=b);
+
+
+//comparision operators
+//equal
+console.log(a==b);
+//not equal
+console.log(a!=b);
+//strict equal
+console.log(a===b);
+//strict not equal
+console.log(a!==b);
+//greater than
+console.log(a>b);
+//greater than equal
+console.log(a>=b);
+//less than
+console.log(a<b);
+//lesss than equal
+console.log(a<=b);
+
+//arithmatic operators
+//remainder
+console.log(a%b);
+//increment
+console.log(a++);
+//decrement
+console.log(a--);
+//unary negation
+console.log(a-b);
+//unary plus
+console.log(a+b);
+//exponentio operator
+console.log(a**b);
+
+//logical operators
+//AND
+console.log(a&&b);
+//OR
+console.log(a||b);
+//NOT
+console.log(!a);
+
+//bitwise operators
+//AND
+console.log(a&b);
+//OR
+console.log(a|b);
+//NOT
+console.log(~a);
+//XOR
+console.log(a^b);
+//left shift
+console.log(a<<b);
+//right shift
+console.log(a>>b);
+//zero fill
+console.log(a>>>b);
+
+//string operators
+console.log("my"+"string");
+
+//conditional operator or ternary operator
+var age=17;
+
+var sta = (age>=18) ? "adult":"minor";
+
+console.log(sta);
+
+
+
+/// data types
+// number
+console.log(a+b);
+//string
+var d = 10
+var c = "mugesh"
+console.log(d+c);
+//boolean
+console.log(a>b)
+//nan
+console.log(d/c);
+//null
+var f = ""
+console.log(f);
+//undefined
+var country = 
+console.log(country);
+//array
+var cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+//object
+var person = {firstName:"mugesh", lastName:"kumar", age:22, state:"tamilnadu"};
+console.log(person);
